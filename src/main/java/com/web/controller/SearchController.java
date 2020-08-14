a3d90ac8b25d1cdd3a38988c5a77360aa339babe
kxw009988 <15261398617@163.com>
2020-08-14 10:33:17 +0800

    搜索

--- src/main/java/com/web/controller/SearchController.java
+++ src/main/java/com/web/controller/SearchController.java
@@ -2,6 +2,7 @@ package com.web.controller;
 
 import com.web.entity.*;
 import com.web.repository.DocumentationRepository;
+import com.web.repository.GroupMemberRepository;
 import com.web.repository.GroupRepository;
 import com.web.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,20 +14,96 @@ import java.util.List;
 @Controller
 @CrossOrigin
 public class SearchController {
+
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
     DocumentationRepository documentationRepository;
+
+    @Autowired
     GroupRepository groupRepository;
+
+    @Autowired
+    GroupMemberRepository groupMemberRepository;
 
     @GetMapping(value={"/search"})
     @ResponseBody
-    public SearchResult search(@RequestParam int userId, @RequestParam String keyword, @RequestParam int type) {
-        if (type == 0) return searchUser(keyword);
-        else if (type == 1) return searchGroup(keyword);
-        else if (type == 2) return searchDocumentation(keyword);
-        return null;
+    public SearchResult search(@RequestParam int userId, @RequestParam String keyword) {
+        SearchResult searchResult = new SearchResult();
+        List<Documentation> docList = documentationRepository.findByCreatorId(userId);
+
+        for (Documentation doc : docList) {
+            if (doc.title.contains(keyword)) {
+                DocSearch docSearch = new DocSearch(doc.title, doc.id);
+                searchResult.docResultList.add(docSearch);
+            }
+        }
+
+        List<GroupMember> grpMemberList = groupMemberRepository.findGroupMemberByUserId(userId);
+
+        for (GroupMember grpMember : grpMemberList) {
+            Group grp = groupRepository.findGroupById(grpMember.groupId);
+            if (grp.groupName.contains(keyword)) {
+                GrpSearch grpSearch = new GrpSearch(grp.groupName, grp.id);
+                searchResult.grpResultList.add(grpSearch);
+            }
+        }
+
+        return searchResult;
     }
 
+    @GetMapping(value={"/searchDocThroughUsr"})
+    @ResponseBody
+    public MyDocResult searchDocThroughUsr(@RequestParam String username) {
+        User user = userRepository.findUserByUsername(username);
+        MyDocResult myDocResult = new MyDocResult();
+
+        if (user == null) {
+            myDocResult.docResultList = null;
+            return myDocResult;
+        }
+
+        List<Documentation> docList = documentationRepository.findByCreatorId(user.id);
+
+        if (!docList.isEmpty()) {
+            for (Documentation doc : docList) {
+                if (!doc.isTrash) {
+                    DocSearch docSearch = new DocSearch(doc.title, doc.id);
+                    myDocResult.docResultList.add(docSearch);
+                }
+            }
+        }
+
+        return myDocResult;
+    }
+
+    @GetMapping(value={"/searchTrashThroughUsr"})
+    @ResponseBody
+    public MyDocResult searchTrashThroughUsr(@RequestParam String username) {
+        User user = userRepository.findUserByUsername(username);
+        MyDocResult myDocResult = new MyDocResult();
+
+        if (user == null) {
+            myDocResult.docResultList = null;
+            return myDocResult;
+        }
+
+        List<Documentation> docList = documentationRepository.findByCreatorId(user.id);
+
+        if (!docList.isEmpty()) {
+            for (Documentation doc : docList) {
+                if (doc.isTrash) {
+                    DocSearch docSearch = new DocSearch(doc.title, doc.id);
+                    myDocResult.docResultList.add(docSearch);
+                }
+            }
+        }
+
+        return myDocResult;
+    }
+
+    /*
     public SearchResult searchUser(@RequestParam String username) {
         User user = userRepository.findUserByUsername(username);
         SearchResult searchResult = new SearchResult();
@@ -67,54 +144,5 @@ public class SearchController {
         return searchResult;
     }
 
-    @GetMapping(value={"/searchDocThroughUsr"})
-    @ResponseBody
-    public SearchResult searchDocThroughUsr(@RequestParam String username) {
-        User user = userRepository.findUserByUsername(username);
-        SearchResult searchResult = new SearchResult();
-
-        if (user == null) {
-            searchResult.resultList = null;
-            return searchResult;
-        }
-
-        List<Documentation> docList = documentationRepository.findByCreatorId(user.id);
-
-        if (!docList.isEmpty()) {
-            for (Documentation doc : docList) {
-                if (!doc.isTrash) {
-                    DocSearch docSearch = new DocSearch(doc.title, doc.id);
-                    searchResult.resultList.add(docSearch);
-                }
-            }
-        }
-
-        return searchResult;
-    }
-
-    @GetMapping(value={"/searchTrashThroughUsr"})
-    @ResponseBody
-    public SearchResult searchTrashThroughUsr(@RequestParam String username) {
-        User user = userRepository.findUserByUsername(username);
-        SearchResult searchResult = new SearchResult();
-
-        if (user == null) {
-            searchResult.resultList = null;
-            return searchResult;
-        }
-
-        List<Documentation> docList = documentationRepository.findByCreatorId(user.id);
-
-        if (!docList.isEmpty()) {
-            for (Documentation doc : docList) {
-                if (doc.isTrash) {
-                    DocSearch docSearch = new DocSearch(doc.title, doc.id);
-                    searchResult.resultList.add(docSearch);
-                }
-            }
-        }
-
-        return searchResult;
-    }
-
+     */
 }
---
+++ src/main/java/com/web/entity/GrpSearch.java
@@ -0,0 +1,13 @@
+package com.web.entity;
+
+public class GrpSearch {
+    public String title;
+    public int id;
+
+    public GrpSearch() {}
+
+    public GrpSearch(String tle, int ID) {
+        title = tle;
+        id = ID;
+    }
+}
