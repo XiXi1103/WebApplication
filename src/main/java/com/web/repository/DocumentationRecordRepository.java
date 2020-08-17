2f3c3b3d18a15d5d51ec39a163e8b315b12582ac
nnddss <1076645646@qq.com>
2020-08-17 16:15:24 +0800

    最近浏览

--- src/main/java/com/web/repository/DocumentationRecordRepository.java
+++ src/main/java/com/web/repository/DocumentationRecordRepository.java
@@ -1,5 +1,6 @@
 package com.web.repository;
 
+import ch.qos.logback.core.boolex.EvaluationException;
 import com.web.entity.Collaborator;
 import com.web.entity.DocumentationRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +11,7 @@ import java.util.List;
 
 public interface DocumentationRecordRepository extends JpaRepository<DocumentationRecord,Integer> {
     ArrayList<DocumentationRecord> findDocumentationRecordByDocumentationId(int docId);
+    //@Query(value="select * from  DocumentationRecord order by 'time' desc", nativeQuery=true)
     ArrayList<DocumentationRecord> findDocumentationRecordByUserId(int userId);
     DocumentationRecord findDocumentationRecordByUserIdAndDocumentationId(int userId,int docId);
 }
